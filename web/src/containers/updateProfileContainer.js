95c2be28eef3fe11d36dea4344763cb0a70f09d7
Ata Bekiroglu <abekiroglu14@ku.edu.tr>
2019-11-29 18:34:53 +0300

    update profile-container fix missing fields

--- web/src/containers/updateProfileContainer.js
+++ web/src/containers/updateProfileContainer.js
@@ -18,106 +18,108 @@ import { getMe, updateProfileCompany } from '../actions/company';
 
 
 class UpdateProfileContainer extends Component {
- constructor(props) {
-    super(props);
-    this.state = {
-        description: null,
-        companyName: null,
-        foundedDate: null
+    constructor(props) {
+        super(props);
+        this.state = {
+            description: null,
+            name: null,
+            foundedDate: null
+        };
     };
-};
 
-setCompanyName = e => {
-    this.setState({ companyName: e.target.value });
-};
+    setname = e => {
+        this.setState({ name: e.target.value });
+    };
 
-setFoundedDate = e => {
-    this.setState({ foundedDate: e.target.value });
-};
-setDescription = e => {
-    this.setState({ description: e.target.value });
-};
+    setFoundedDate = e => {
+        var dateArr = e.target.value.split('-');
+        var formattedDate = `${dateArr[2]}-${dateArr[1]}-${dateArr[0]}`
+        this.setState({ foundedDate: formattedDate });
+    };
+    setDescription = e => {
+        this.setState({ description: e.target.value });
+    };
 
-componentDidMount() {
-    const { getMe } = this.props;
-    getMe();
-    //loginCompany({ email: 'testcompany@company.com', password: 'ezjobs' })
-    //this.redirectToMenu();
-}
+    componentDidMount() {
+        const { getMe } = this.props;
+        getMe();
+        //loginCompany({ email: 'testcompany@company.com', password: 'ezjobs' })
+        //this.redirectToMenu();
+    }
 
-render() {
-    return (
-       
-    <div>
-        <div className="App__Form">
-            <NavLink exact to="/main" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Home Page</NavLink> 
-        </div>
-        <div>
-             {this.props.isLoading ? 'Loading...' : null}
-             {this.props.hasError ? <ErrorField error={this.props.error.message} /> : null}
-             {this.props.company ? <div> Welcome {this.props.company.email} </div> : null}
+    render() {
+        return (
+
+            <div>
+                <div className="App__Form">
+                    <NavLink exact to="/main" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Home Page</NavLink>
+                </div>
+                <div>
+                    {this.props.isLoading ? 'Loading...' : null}
+                    {this.props.hasError ? <ErrorField error={this.props.error.message} /> : null}
+                    {this.props.company ? <div> Welcome {this.props.company.email} </div> : null}
+                </div>
+                <span className="login-page-title"> Design Profile </span>
+                <form onSubmit={this.onClickSubmit}>
+                    <div className="center">
+                        <div className="login-form-div" />
+                        <Row>
+                            <Col lg="5" className="text-center text-md-left">
+                                <div className="login-form-div">
+                                    <h5>Company Name:</h5>
+                                </div>
+                            </Col>
+                            <Col lg="5" className="text-center text-md-right">
+                                <div className="login-form-div">
+                                    {this.props.company ? <CompanyNameInput onChange={this.setname} placeholder={this.props.company.name} style={{ width: 300 }} /> : null}
+                                </div>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col lg="5" className="text-center text-md-left">
+                                <div className="login-form-div">
+                                    <h5>Established Date:</h5>
+                                </div>
+                            </Col>
+                            <Col lg="5" className="text-center text-md-right">
+                                <div className="login-form-div">
+                                    <DateInput onChange={this.setFoundedDate} style={{ width: 300 }} />
+                                </div>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col lg="5" className="text-center text-md-left">
+                                <div className="login-form-div">
+                                    <h5>Description Of Company:</h5>
+                                </div>
+                            </Col>
+                            <Col lg="5" className="text-center text-md-right">
+                                <div className="login-form-div">
+                                    <TextAreaInput onChange={this.setDescription} />
+                                </div>
+                            </Col>
+                        </Row>
+                        <div className="login-form-div" />
+                        <button type="submit" className="btn btn-success"> Update </button>
+                        <ErrorField error={this.props.error} />
+                    </div>
+                </form>
             </div>
-        <span className="login-page-title"> Design Profile </span>
-        <form onSubmit={this.onClickSubmit}>
-        <div className="center">
-        <div className="login-form-div" />
-            <Row>
-                <Col lg="5" className="text-center text-md-left">
-                    <div className="login-form-div">
-                        <h5>Company Name:</h5>
-                    </div>
-                </Col>
-                <Col lg="5" className="text-center text-md-right">
-                    <div className="login-form-div">
-                        <CompanyNameInput onChange={this.setCompanyName} placeholder={this.state.company.companyName}  style={{width: 300}}/>
-                    </div>
-                </Col>
-            </Row>
-            <Row>
-                <Col lg="5" className="text-center text-md-left">
-                    <div className="login-form-div">
-                        <h5>Established Date:</h5>
-                    </div>
-                </Col>
-                <Col lg="5" className="text-center text-md-right">
-                    <div className="login-form-div">
-                        <DateInput onChange={this.setFoundedDate} style={{width: 300}}/>
-                    </div>
-                </Col>
-            </Row>
-            <Row>
-                <Col lg="5" className="text-center text-md-left">
-                    <div className="login-form-div">
-                        <h5>Description Of Company:</h5>
-                    </div>
-                </Col>
-                <Col lg="5" className="text-center text-md-right">
-                    <div className="login-form-div">
-                        <TextAreaInput onChange={this.setDescription}/>
-                    </div>
-                </Col>
-            </Row>
-            <div className="login-form-div" />
-            <button type="submit" className="btn btn-success"> Update </button>
-            <ErrorField error={this.props.error} />  
-            </div> 
-        </form>
-    </div>
-    );
-}
+        );
+    }
 
-onClickSubmit = e => {
-    e.preventDefault();
+    onClickSubmit = e => {
+        e.preventDefault();
         const { updateProfileCompany } = this.props;
-        const { companyName, foundedDate, description} = this.state;
+        const { name, foundedDate, description } = this.state;
         let body = {
-        companyName: companyName,
-        foundedDate: foundedDate,
-        description : description
+            name: name,
+            foundedDate: foundedDate,
+            description: description
         };
         updateProfileCompany(body);
-    
-};
+
+    };
 }
 
 const mapStateToProps = state => {
