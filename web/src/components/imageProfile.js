ebce7c5dac04fb8592733e20977a9d51d8d3f168
ali <a.mufettisoglu@gmail.com>
2019-11-29 15:26:21 +0300

    .

---
+++ web/src/components/imageProfile.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import '../styles/emailInput.css';
+
+export class ImageProfile extends Component {
+  render() {
+    // return (
+    //     <img src="..." alt="..." class="img-thumbnail">
+    // );
+  }
+}
